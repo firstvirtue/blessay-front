5d0a732e9261ca31e3868fd80ba7179e7f1fdc62
Jake Lee <jake@iropke.com>
2020-02-09 00:08:15 +0900

    Init redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { Home, Auth } from 'pages';
+
+function App() {
+  return (
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/auth" component={Auth}/>
+    </div>
+  );
+}
+
+export default App;
