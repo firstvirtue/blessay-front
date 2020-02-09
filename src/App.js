00ae9dc9931098fd12b85b4991355880ee77659e
Jake Lee <jake@iropke.com>
2020-02-09 22:36:04 +0900

    Add login header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { Home, Auth } from 'pages';
+import HeaderContainer from 'containers/Base/HeaderContainer';
 
 function App() {
   return (
     <div>
+      <HeaderContainer/>
       <Route exact path="/" component={Home}/>
       <Route path="/auth" component={Auth}/>
     </div>
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as baseActions from 'redux/modules/base';
 
 class Auth extends Component {
+  // 페이지에 진입 할 때 헤더를 비활성화
+  componentWillReceiveProps(nextProps) {
+    console.log(1);
+    this.props.BaseActions.setHeaderVisibility(false);
+  }
+
+//   static getDerivedStateFromProps(nextProps, prevState) {
+//     if (nextProps.name !== prevState.name) {
+//         return { name: nextProps.name, lastTime: Date.now() };
+//     }
+//     return null;
+// }
+
+  // 페이지에서 벗어 날 때 다시 활성화
+  componentWillUnmount() {
+    this.props.BaseActions.setHeaderVisibility(true);
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +31,11 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+export default connect(
+  (state) => ({
+
+  }),
+  (dispatch) => ({
+    BaseActions: bindActionCreators(baseActions, dispatch)
+  })
+)(Auth);
