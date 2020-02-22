0127922847db8c494d3bbd06c283a159702c5bf0
Jake Lee <jake@iropke.com>
2020-02-23 01:14:09 +0900

    Add user state UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Route } from 'react-router-dom';
 import { Home, Auth } from 'pages';
 import HeaderContainer from 'containers/Base/HeaderContainer';
 
+import storage from 'lib/storage';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as userActions from 'redux/modules/user';
+
 function App() {
   return (
     <div>
--- src/containers/Base/HeaderContainer.js
+++ src/containers/Base/HeaderContainer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Header, { LoginButton } from 'components/Base/Header';
 import { connect } from 'react-redux';
+import * as userActions from 'redux/modules/user';
+import { bindActionCreators } from 'redux';
+import storage from 'lib/storage';
 
 class HeaderContainer extends Component {
   // constructor() {
@@ -8,14 +11,31 @@ class HeaderContainer extends Component {
   //   console.log('header');
   // }
 
+  handleLogout = async () => {
+    const { UserActions } = this.props;
+    try {
+      await UserActions.logout();
+    } catch (e) {
+      console.log(e);
+    }
+
+    storage.remove('loggedInfo');
+    window.location.href = '/';
+  }
+
   render() {
-    const { visible } = this.props;
-    // console.log(visible);
+    const { visible, user } = this.props;
     if(!visible) return null;
 
     return (
       <Header>
-        <LoginButton/>
+        { user.get('logged') 
+          ? (<div>
+            { user.getIn(['loggedInfo', 'username'])} <div onClick={this.handleLogout}>(로그아웃)</div>
+          </div>)
+          : 
+          <LoginButton/>
+        }
       </Header>
     );
   }
@@ -23,9 +43,10 @@ class HeaderContainer extends Component {
 
 export default connect(
   (state) => ({
-    visible: state.base.getIn(['header', 'visible'])
+    visible: state.base.getIn(['header', 'visible']),
+    user: state.user
   }),
   (dispatch) => ({
-
+    UserActions: bindActionCreators(userActions, dispatch)
   })
 )(HeaderContainer);
