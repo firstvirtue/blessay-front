9b5426630e9e4b3b1b65d2920339c786ec55b528
Jake Lee <firstvirtue@gmail.com>
2020-02-28 00:58:25 +0900

    Add editorjs

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,3 +1,4 @@
 export { default as PostWrapper } from './PostWrapper';
 export { default as RouteLink } from './RouteLink';
 export { default as PostLink } from './PostLink';
+export { default as PostButton } from './PostButton';
--- src/containers/Post/Edit.js
+++ src/containers/Post/Edit.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { PostWrapper, RouteLink } from 'components/Post';
+import { PostWrapper, RouteLink, PostButton } from 'components/Post';
+import EditorJS from '@editorjs/editorjs';
 
 class Edit extends Component {
+  constructor(props) {
+    super(props);
+
+    const editor = new EditorJS({
+      holderId: 'container',
+      autofocus: true,
+    });
+  }
+
+  handleSave = async () => {
+    alert(1);
+  }
 
   render() {
+    const { handleSave } = this;
 
     return (
       <PostWrapper>
-        
+        <PostButton onClick={handleSave}>저장하기</PostButton>
+        <RouteLink to="/post/list">취소하기</RouteLink>
+        <div id="container"></div>
       </PostWrapper>
     );
   }
